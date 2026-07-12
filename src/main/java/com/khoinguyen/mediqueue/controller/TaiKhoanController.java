af22b943043febe201e8d321773828291ade7b30
Khoi Nguyen <khoinguyenag2004@gmail.com>
2026-07-13 06:55:13 +0700

    Loại bỏ các phần dư

--- src/main/java/com/khoinguyen/mediqueue/controller/TaiKhoanController.java
+++ src/main/java/com/khoinguyen/mediqueue/controller/TaiKhoanController.java
@@ -3,14 +3,11 @@ package com.khoinguyen.mediqueue.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.khoinguyen.mediqueue.entity.TaiKhoan;
